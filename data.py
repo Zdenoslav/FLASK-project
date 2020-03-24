b0111b0b39360a367ee11318bb8a03763155c865
ZDENKO MUCINA <MucinaZ@cardiff.ac.uk>
2020-03-24 18:31:59 +0000

    changes to the add file, adding new routes

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'lorem ipsum dolor lenos popspos  psopos ha pa oa h apa j ka; ap ah',
+            'author':'Zdenko Mucina',
+            'create_date': '04-04-3018'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'lorem ipsum dolor lenos popspos  psopos ha pa oa h apa j ka; ap ah',
+            'author':'Lorna Cassar',
+            'create_date': '04-05-3018'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'lorem ipsum dolor lenos popspos  psopos ha pa oa h apa j ka; ap ah',
+            'author':'Georfe Oreejkk',
+            'create_date': '04-05-3018'
+        },
+    ]
+    return articles
